0c37281a33ce67cf245dec99ee24525bc8aec038
Radhey <radheysaraan@gmail.com>
2025-02-20 19:20:33 +0530

    ignition

--- App.js
+++ App.js
@@ -1,4 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const rootElement = document.getElementById('root');
-const element = React.createElement('h1', {id:"heading", xyz:"abc"}, 'Hello, world!');
+const parent = React.createElement('div', {id:"parent"},[
+        React.createElement('h1', {id:"heading"}, "Hello World"),
+        React.createElement('h2', {id:"subheading"}, "This is a React App")
+])
+
+// JSX
+
 const root = ReactDOM.createRoot(rootElement);
-root.render(element);
+root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Next_Orbit
+
+...
+# parcel
+- Dev build
+- local server
+- hmr -> hot module replacement
+- file watcher algorithm - written in C++
+- faster build - because caching
+- image optimization
+- minification - prod
+- bundle - prod
+- compressing - prod
+- consistance hashing
+- code split
+- differential bundling -> to support older browsers
+- diagonstic
+- error handling
+- https
+- Tree shaking, remove unused code
+- Different dev and prod bundles
+
+
